a37f90177e818b097999eecca5be4f541cbb1e5a
Govind Malurwar <govind.mallurwar@gmail.com>
2026-05-03 09:51:55 +0100

    add gitignore and initial test case

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,5 +1,7 @@
 package uk.gov.dwp.uc.pairtest;
 
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
@@ -7,10 +9,22 @@ public class TicketServiceImpl implements TicketService {
     /**
      * Should only have private methods other than the one below.
      */
+    private static final int ADULT_TICKET_PRICE = 25;
+
+    private final TicketPaymentService paymentService;
+    private final SeatReservationService reservationService;
+
+    public TicketServiceImpl(TicketPaymentService paymentService, SeatReservationService reservationService) {
+        this.paymentService = paymentService;
+        this.reservationService = reservationService;
+    }
 
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+        int adultTickets = ticketTypeRequests[0].getNoOfTickets();
 
+        paymentService.makePayment(accountId, adultTickets * ADULT_TICKET_PRICE);
+        reservationService.reserveSeat(accountId, adultTickets);
     }
 
 }
